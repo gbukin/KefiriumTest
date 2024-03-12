0d9aeb3db9b76f1a5a3f6917a5b0c38e4ae19e98
gnusik533 <asgargion14@gmail.com>
2024-03-13 02:42:45 +0400

    Обновил .env.example: добавил ключи для Google oAuth
    Обновил entrypoint: добавил пост-скрипты для создания ENV и создания ключа-приложения

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@ cd /app || exit
 
 composer install && npm install && npm run build
 
+composer run post-root-package-install
+composer run post-create-project-cmd
+
 test_status=$(php artisan test | grep "fail")
 if [ ${#test_status} != "0" ]; then
   echo "PHPUnit test fail!";
