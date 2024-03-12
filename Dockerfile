c7d68342ff2be75985f1d15e1d91275bfd97c08e
gnusik533 <asgargion14@gmail.com>
2024-03-12 19:37:35 +0400

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+FROM php:8.2.8-fpm-alpine3.18
+
+ENV PHP_VERSION=8.2.8
+
+RUN addgroup -S -g 1000 www \
+ && adduser -S -D -u 1000 -s /sbin/nologin -h /app -G www www
+
+RUN apk add --update --no-cache \
+	supervisor \
+	nginx \
+	curl-dev \
+	nodejs \
+	npm \
+	libpq-dev \
+	bash
+
+RUN docker-php-ext-install pdo_pgsql pgsql curl \
+    && docker-php-ext-enable pdo_pgsql \
+    && rm -rf /var/cache/apk/*
+
+RUN curl -s https://getcomposer.org/installer | php \
+    && alias composer='php composer.phar' && mv composer.phar /usr/bin/composer
+
+EXPOSE 9000/tcp
+
+EXPOSE 443 80
+
+WORKDIR /app
+
+CMD ["/usr/bin/supervisord", "-c", "/etc/supervisord.conf"]
---
+++ application/routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    if (\Illuminate\Support\Facades\Auth::check()) {
+        return redirect()->route('dashboard');
+    } else {
+        return redirect()->route('login');
+    }
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/google/redirect', [App\Http\Controllers\GoogleLoginController::class, 'redirectToGoogle'])->name('google.redirect');
+Route::get('/google/callback', [App\Http\Controllers\GoogleLoginController::class, 'handleGoogleCallback'])->name('google.callback');
+
+require __DIR__.'/auth.php';
